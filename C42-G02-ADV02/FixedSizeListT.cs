64b373017cc9e71fac5ca6af90edb16fb97d8618
mali84211 <mali84211@gmail.com>
2024-08-01 20:38:35 +0300

    all ass

---
+++ C42-G02-ADV02/FixedSizeListT.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C42_G02_ADV02
+{
+    public class FixedSizeList<T>
+    {
+        private T[] items;
+        private int count;
+
+        public int Capacity { get; private set; }
+        public int Count { get { return count; } }
+
+        public FixedSizeList(int capacity)
+        {
+            if (capacity <= 0)
+            {
+                throw new ArgumentException("Capacity must be greater than zero.");
+            }
+
+            Capacity = capacity;
+            items = new T[capacity];
+            count = 0;
+        }
+
+        public void Add(T item)
+        {
+            if (count >= Capacity)
+            {
+                throw new InvalidOperationException("Cannot add more elements, the list is full.");
+            }
+
+            items[count] = item;
+            count++;
+        }
+
+        public T Get(int index)
+        {
+            if (index < 0 || index >= count)
+            {
+                throw new IndexOutOfRangeException("Index is out of range.");
+            }
+
+            return items[index];
+        }
+    }
+}
