2cbd1c5061c01af225749aee28dc0528ca420086
mali84211 <mali84211@gmail.com>
2024-08-01 21:00:10 +0300

    Arraylist

--- C42-G02-ADV02/Program.cs
+++ C42-G02-ADV02/Program.cs
@@ -1,10 +1,63 @@
-﻿namespace C42_G02_ADV02
+﻿using System.Collections;
+
+namespace C42_G02_ADV02
 {
     internal class Program
     {
+        //public static int SumarrayList(ArrayList arrayList) {
+        //    int Sum=0;
+        //    if (arrayList is not null)
+        //    {
+        //        for (int i = 0; i <arrayList.Count; i++)
+        //        {
+        //            Sum +=(int) arrayList[i];
+        //        }
+        //        return Sum;
+        //    }
+        //    return Sum;
+        //}
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region V1
+            //ArrayList arrayList = new ArrayList();
+            //Console.WriteLine($"Count of arraylist={arrayList.Count},Capacity of arraylist={arrayList.Capacity}");
+            //arrayList.Add( 1 );
+            //Console.WriteLine("After Adding First Element");
+            //Console.WriteLine($"Count of arraylist={arrayList.Count},Capacity of arraylist={arrayList.Capacity}");
+            //arrayList.Add(2);
+            //arrayList.AddRange(new int[] {3,4} );
+            //Console.WriteLine($"Count of arraylist={arrayList.Count},Capacity of arraylist={arrayList.Capacity}");
+            //arrayList.Add(5);
+            //Console.WriteLine("After Adding 5th Element");
+            //Console.WriteLine($"Count of arraylist={arrayList.Count},Capacity of arraylist={arrayList.Capacity}");
+
+            //ArrayList arrayList = new ArrayList(5) {1,2,3,4,5};
+            //Console.WriteLine($"Count of arraylist={arrayList.Count},Capacity of arraylist={arrayList.Capacity}");
+            //arrayList.Add(6);
+            //Console.WriteLine("After Adding 6th Element");
+            //Console.WriteLine($"Count of arraylist={arrayList.Count},Capacity of arraylist={arrayList.Capacity}");
+            //arrayList.TrimToSize();
+            //Console.WriteLine("After Trim");
+            //Console.WriteLine($"Count of arraylist={arrayList.Count},Capacity of arraylist={arrayList.Capacity}");
+            //arrayList.Add(1);
+            //arrayList.Add("mo");
+            //foreach (int item in arrayList)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            #endregion
+
+            #region
+            #endregion
+
+            #region
+            #endregion
+
+            #region
+            #endregion
+
+            #region
+            #endregion
         }
     }
 }
